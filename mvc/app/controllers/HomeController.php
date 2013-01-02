7fc57754b4ae2c7a611b31dc798dc18b486344d7
James Holdren <jamesdholdren@gmail.com>
2013-01-01 22:50:36 -0500

    Cleaned up test code

--- mvc/app/controllers/HomeController.php
+++ mvc/app/controllers/HomeController.php
@@ -11,14 +11,8 @@ class HomeController extends Controller
 		parent::__construct();
 	}
 
-	public function IndexAction($variable)
+	public function IndexAction()
 	{
-		echo $variable;
-		//$this->load->render('Demo', array('message' => 'Looks like your Mindless MVC installation is working!'), true);
-	}
-
-	public function HelloAction($something)
-	{
-		echo $something;
+		$this->load->render('Demo', array('message' => 'Looks like your Mindless MVC installation is working!'), true);
 	}
 }
