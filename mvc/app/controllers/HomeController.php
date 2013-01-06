df83819eda7151055df43bb9dc64734ff8e3a847
James Holdren <jamesdholdren@gmail.com>
2013-01-05 22:38:11 -0500

    Commented the default action

--- mvc/app/controllers/HomeController.php
+++ mvc/app/controllers/HomeController.php
@@ -13,6 +13,7 @@ class HomeController extends Controller
 
 	public function IndexAction()
 	{
+		// This is a sample rendering of a template using some dummy data
 		$this->load->render('Demo', array('message' => 'Looks like your Mindless MVC installation is working!'), true);
 	}
 }
